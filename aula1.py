91d7c6f06d3691e122a056764d2524fc036d23ca
BiiahBraga <beatrizsagrado9@gmail.com>
2025-03-29 09:53:32 -0300

    update - aula1 e aula2

---
+++ aula1.py
@@ -0,0 +1,17 @@
+# Permite escrever um comentário
+
+print('Hello, world!') # Imprimi algo
+print("It's me!")
+print(123)
+
+
+"""
+Isso não é um comentário, é um docstring
+Agora pode pular linhas
+O interpretador do python lê isso!
+"""
+
+''''
+Usar para escrever notas e explicar códigos
+Também é uma docstring
+'''
